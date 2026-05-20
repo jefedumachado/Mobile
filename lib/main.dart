125b5418edb15fe5d5de400af4f26a7a607f5868
Laura Donath <lauradonath11@gmail.com>
2026-05-19 21:07:14 -0300

    feat: implement button component

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:dev_venture/theme/dark_theme.dart';
-import 'package:dev_venture/theme/light_theme.dart';
-import 'package:dev_venture/screens/theme_demo.dart';
 import 'package:flutter/material.dart';
+import 'components/button_component.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,72 +8,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppLightTheme.theme,
-      darkTheme: AppDarkTheme.theme,
-      themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            tooltip: 'Open theme demo',
-            icon: const Icon(Icons.palette),
-            onPressed: () {
-              Navigator.of(
-                context,
-              ).push(MaterialPageRoute(builder: (_) => const ThemeDemoPage()));
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Button Component'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: ButtonComponent(
+              text: 'Continuar',
+              icon: Icons.arrow_forward,
+              onPressed: () {
+                print('Botão clicado!');
+              },
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
-  }
-}
