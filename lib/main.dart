747548f1ba75b3b7bac1d13dcbf947568d0cc448
Laura Donath <lauradonath11@gmail.com>
2026-05-25 20:08:18 -0300

    Implements tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'components/button_component.dart';
+import 'package:dev_venture/screens/cadastro_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Button Component'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Center(
-            child: ButtonComponent(
-              text: 'Continuar',
-              icon: Icons.arrow_forward,
-              onPressed: () {
-                print('Botão clicado!');
-              },
-            ),
-          ),
-        ),
-      ),
+      home: const CadastroScreen(),
     );
+  }
+}
